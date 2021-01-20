123c4dfc7c5fd094eb3c02be671a1f9abd9a4a37
Evgeniya Selivanova <kulbatskaya.evgenia@gmail.com>
2021-01-20 09:46:06 +0300

    6.1_First Version.3

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,72 +2,69 @@ package ru.netology.stats;
 
 public class StatsService {
 
-    public long totalSumMonth(long SumMonths[]) {
-        long Sum = 0;
-        for (long SumMonth : SumMonths) {
-            if (SumMonth >= 0) {
-                Sum += SumMonth;
+    public long totalSumMonth(long sumMonths[]) {
+        long sum = 0;
+        for (long sumMonth : sumMonths) {
+            if (sumMonth >= 0) {
+                sum += sumMonth;
             }
         }
-        return Sum;
+        return sum;
     }
 
-    public long averageSumMonth(long SumMonths[]) {
-        long Month = 0;
-        for (long SumMonth : SumMonths) {
-            Month ++;
-        }
-        return totalSumMonth(SumMonths) / SumMonths.length;
+    public long averageSumMonth(long sumMonths[]) {
+        long month = 0;
+        return totalSumMonth(sumMonths) / sumMonths.length;
     }
 
-    public long maxAmountMonth(long SumMonths[]) {
-        long CurrentMonth = SumMonths[0];
-        long Month = 0;
-        long MaxMonth = 0;
-        for (long SumMonth : SumMonths) {
-            Month ++;
-            if (CurrentMonth <= SumMonth) {
-                CurrentMonth = SumMonth;
-                MaxMonth = Month;
+    public long maxAmountMonth(long sumMonths[]) {
+        long currentMonth = sumMonths[0];
+        long month = 0;
+        long maxMonth = 0;
+        for (long sumMonth : sumMonths) {
+            month++;
+            if (currentMonth <= sumMonth) {
+                currentMonth = sumMonth;
+                maxMonth = month;
             }
         }
-        return MaxMonth;
+        return maxMonth;
     }
 
-    public long minAmountMonth(long SumMonths[]) {
-        long CurrentMonth = SumMonths[0];
-        long Month = 0;
-        long MinMonth = 0;
-        for (long SumMonth : SumMonths) {
-            Month ++;
-            if (CurrentMonth >= SumMonth) {
-                CurrentMonth = SumMonth;
-                MinMonth = Month;
+    public long minAmountMonth(long sumMonths[]) {
+        long currentMonth = sumMonths[0];
+        long month = 0;
+        long minMonth = 0;
+        for (long sumMonth : sumMonths) {
+            month++;
+            if (currentMonth >= sumMonth) {
+                currentMonth = sumMonth;
+                minMonth = month;
             }
         }
-        return MinMonth;
+        return minMonth;
     }
 
-    public long calculateAmountMaxMonth(long SumMonths[]) {
-        long CurrentMonth = averageSumMonth(SumMonths);
-        long MaxSumMonth = 0;
-        for (long SumMonth : SumMonths) {
-            if (CurrentMonth < SumMonth) {
-                MaxSumMonth ++;
+    public long calculateAmountMaxMonth(long sumMonths[]) {
+        long currentMonth = averageSumMonth(sumMonths);
+        long maxSumMonth = 0;
+        for (long sumMonth : sumMonths) {
+            if (currentMonth < sumMonth) {
+                maxSumMonth++;
             }
         }
-        return MaxSumMonth;
+        return maxSumMonth;
     }
 
-    public long calculateAmountMinMonth(long SumMonths[]) {
-        long CurrentMonth = averageSumMonth(SumMonths);
-        long MinSumMonth = 0;
-        for (long SumMonth : SumMonths) {
-            if (CurrentMonth > SumMonth) {
-                MinSumMonth ++;
+    public long calculateAmountMinMonth(long sumMonths[]) {
+        long currentMonth = averageSumMonth(sumMonths);
+        long minSumMonth = 0;
+        for (long sumMonth : sumMonths) {
+            if (currentMonth > sumMonth) {
+                minSumMonth++;
             }
         }
-        return MinSumMonth;
+        return minSumMonth;
     }
 
 }
